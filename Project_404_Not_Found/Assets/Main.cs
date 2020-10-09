3bee57d16b7d9be12aa952095416edad52981196
agil17 <agil17@calstatela.edu>
2020-10-09 03:32:34 -0700

    Added Tutorial Stuff From Canvas Module

---
+++ Project_404_Not_Found/Assets/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space Key");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Holding Down E");
+        }
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            Debug.Log("F Key Released");
+        }
+    }
+}
---
+++ Project_404_Not_Found/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * speed * Time.deltaTime);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, verticalInput) * speed * Time.deltaTime);
+    }
+}
